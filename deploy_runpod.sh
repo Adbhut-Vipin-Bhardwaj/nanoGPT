6dd4b568f4df9b31a4262d8d2ff64ddca343a651
Adbhut <adbhut.bhardwaj1410@gmail.com>
2025-03-03 23:16:49 +0530

    Fix scp permissions

--- deploy_runpod.sh
+++ deploy_runpod.sh
@@ -4,7 +4,7 @@ RUNPOD_HOST=$1
 RUNPOD_PORT=$2
 RUNPOD_PROJECT_NAME=$3
 
-scp -P ${RUNPOD_PORT} ./setup_runpod.sh root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
+scp -P ${RUNPOD_PORT} -p ./setup_runpod.sh root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
 scp -P ${RUNPOD_PORT} -r ./datasets root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
 scp -P ${RUNPOD_PORT} *.py root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
 scp -P ${RUNPOD_PORT} requirements.txt root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
