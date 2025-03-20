a92534f9b3bc27524160321cc3621122589fd5a2
Adbhut <adbhut.bhardwaj1410@gmail.com>
2025-03-20 23:10:57 +0530

    Correct dataset dir name in deploy_runpod.sh

--- deploy_runpod.sh
+++ deploy_runpod.sh
@@ -5,6 +5,6 @@ RUNPOD_PORT=$2
 RUNPOD_PROJECT_NAME=$3
 
 scp -P ${RUNPOD_PORT} -p ./setup_runpod.sh root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
-scp -P ${RUNPOD_PORT} -r ./datasets root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
+scp -P ${RUNPOD_PORT} -r ./datasets_dir root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
 scp -P ${RUNPOD_PORT} *.py root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
 scp -P ${RUNPOD_PORT} requirements.txt root@${RUNPOD_HOST}:~/app/${RUNPOD_PROJECT_NAME}
